d4b30d007ce6480a0968013d27bcfc912588f703
Manivannan V <vemani@gmail.com>
2018-03-02 17:32:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  'Ariticle One will be shown here';
+  res.send('Ariticle One will be shown here');
 });
 
 app.get('/ui/madi.png', function (req, res) {
