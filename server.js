1534bc1166e5161d1cfb68179f35310d85cd0ac3
Manivannan V <vemani@gmail.com>
2018-03-06 13:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ function createTemplate(data) {
 var title = data.title;
 var heading = data.heading;
 var date = data.date;
-var content = data;content;
+var content = data.content;
 var htmlTemplate = `
 <html>
    <head>
