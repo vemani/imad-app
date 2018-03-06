882cc1d572b6c93d0b7d700df42bdd7a0cb7991f
Manivannan V <vemani@gmail.com>
2018-03-06 16:06:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = "New Values";
+var img = document.getElementById("mod");
+img.onclick = function(){
+    img.style.marginleft="100px";
+};
