2ac5425f4d7bde67c2a93f21133201d0340ee559
Manivannan V <vemani@gmail.com>
2018-03-07 16:50:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
 // Test js file..
+var xyz = document.getElementById('counter');
+
+xyz.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    // Capture the response value
+    request.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString(); 
+            }
+            
+        }
+        //not done
+    };
+    //Make the request
+    
+request.open('GET', 'http://vemani.imad.hasura-app.io/counter', true);
+request.send(null);
+
+};
