1d9d98898671887bbf6704053e4df283509c32ac
Manivannan V <vemani@gmail.com>
2018-03-07 14:49:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,15 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    //Make a request to counter
+    
+    // Capture the response value
+    
+    //Render the response value
+    
+counter = counter+1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 
-var element = document.getElementById("main-text");
-element.innerHTML = "New Values";
-var img = document.getElementById("mod");
-img.onclick = function(){
-    img.style.marginleft="100px";
+    
 };
- 
